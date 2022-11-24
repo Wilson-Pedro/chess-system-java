122a6b01e5ee1b26064811ff0a4db9c1aae6c52a
Wilson-Pedro <wilsonpedroneto@gmail.com>
2022-11-24 19:23:51 -0300

    Move Pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -44,6 +44,19 @@ public class Board {
 		pieces[position.getRow()][position.getColum()] = piece;
 		piece.position = position;
 	}
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColum()] = null;
+		return aux;
+	}
 	
 	//METODO PARA VER AS POSIÇÕES DO TABULEIRO
 	private boolean positionExists(int row, int column) {
